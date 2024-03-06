8fddf3af281888abe4c5ab49bd2b72da184b917c
jaha-sudo <romanovajaha@gmail.com>
2024-03-06 16:48:08 +0500

    first commit

---
+++ src/api/restaraunt/routes/restaraunt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaraunt router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restaraunt.restaraunt');
---
+++ src/api/restaraunt/services/restaraunt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaraunt service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restaraunt.restaraunt');
